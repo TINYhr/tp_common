d4e52b28834c7c988ffa26a8d2f74e55fe73020b
An Vo <anvox@users.noreply.github.com>
2019-09-04 16:51:22 +0700

    Extend tz reversed lookup by title (#17)
    * Extend tz reversed lookup by title
    
    * Bump version
    
    * Add tz title reversed lookup for compatible back
    
    * Strip white space
    
    * Fix unit test

--- lib/tp_common/timezones/reversed.rb
+++ lib/tp_common/timezones/reversed.rb
@@ -2,16 +2,16 @@ module TpCommon
   module Timezones
     module Reversed
       def self.[](zone_name)
-        TpCommon::Timezones::REVERSED_LIST_ZONES[similar_key(zone_name).to_sym]
+        TpCommon::Timezones::REVERSED_LIST_ZONES[similar_key(zone_name)]
       end
 
       def self.similar_key(zone_name)
-        zone_name = zone_name.to_s.downcase
+        zone_name = zone_name.to_s.downcase.strip
         if zone_name =~ /^(gmt[-\+]\d+)\.\d+$/
           zone_name.split('.').first
         else
           zone_name
-        end
+        end.to_sym
       end
       private_class_method :similar_key
     end
